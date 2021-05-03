eb6ef2756fbe673ad4c0c32a3776b96600ab0e71
Marcos Vinicius Cassel <mvc.security@gmail.com>
2021-05-03 00:07:42 -0300

    Included function dns_resolve

--- venv/host_footprint.py
+++ venv/host_footprint.py
@@ -4,18 +4,11 @@ import dns.resolver
 from bs4 import BeautifulSoup
 import reg
 import json
+import argparse
+
 
 # Caso eu queira aceitar outro resolv.conf, posso iniciar um resolver com
 # novoResolver = dns.resolver.Resolver("<caminho do novo resolv.conf")
-
-
-
-
-def version():
-    print("host_footprint.py - 0.0.1")
-
-# def help():
-
 
 def resolve_dns(domain_name, query_type="A", verbose=True):
     try:
@@ -44,29 +37,66 @@ def resolve_dns(domain_name, query_type="A", verbose=True):
 # Keep this as TRUE while we do not finish its implementation otherwise it will crash
 verbose = True
 query_types = ["A", 'MX', 'CNAME', 'NS', 'TXT', 'AAAA']
-domain_name = "sgnh.com.br"
 dns_recon_report_file = "dns_registers.txt"
 dns_findings = []
 
-opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-if "-d" in opts:
+args_parser = argparse.ArgumentParser(
+    usage='%(prog)s [options] <domain_name>',
+    description="This is a learning project "
+                "created to help me on my python learning journey, "
+                "mixing security and python programming. "
+                "The intention of this tool, when finished is to provide a "
+                "complete footprint of a given domain or IP",
+    allow_abbrev=False)
+
+args_parser.add_argument('-d',
+                         '--dns',
+                         action='store_true',
+                         help='search all types of dns records for the informed dns_name')
+args_parser.add_argument('--output_file',
+                         action='store',
+                         nargs=1,
+                         default='footprint_report.txt',
+                         help='inform the name of the output file used to record the footprint findings')
+args_parser.add_argument('domain_name',
+                         metavar='domain_name',
+                         action='store')
+
+
+args = args_parser.parse_args()
+domain_name = args.domain_name
+report_file = args.output_file
+if args.dns:
     for type in query_types:
-        dns_answer = resolve_dns(domain_name, type, verbose)
-        if dns_answer is None:
-            continue
-        else:
-            dns_findings.append(dns_answer)
-
+            dns_answer = resolve_dns(domain_name, type, verbose)
+            if dns_answer is None:
+                continue
+            else:
+                dns_findings.append(dns_answer)
 
     with open(dns_recon_report_file, 'w') as dns_file_writer:
         for dns_register in dns_findings:
             dns_file_writer.writelines(dns_register.__str__())
             dns_file_writer.write('\n')
-else:
-    raise SystemExit(f"Usage: {sys.argv[0]} (-d)")
 
+
+print(args)
+# if "-d" in opts:
+#     for type in query_types:
+#         dns_answer = resolve_dns(domain_name, type, verbose)
+#         if dns_answer is None:
+#             continue
+#         else:
+#             dns_findings.append(dns_answer)
+#
+#     with open(dns_recon_report_file, 'w') as dns_file_writer:
+#         for dns_register in dns_findings:
+#             dns_file_writer.writelines(dns_register.__str__())
+#             dns_file_writer.write('\n')
+# else:
+#     raise SystemExit(help())
+#
 
 
 
